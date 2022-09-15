738e884770baa123b1167c8f4c863a12b3eaeb19
Codehearl <sanusiadeyemi66@gmail.com>
2022-09-15 11:25:35 +0100

    added web layout

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_ui_clone/colors.dart';
 import 'package:whatsapp_ui_clone/info.dart';
 
 class ContactList extends StatelessWidget {
@@ -6,26 +7,38 @@ class ContactList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 4),
-      child: ListView.builder(
-          itemCount: info.length,
-          shrinkWrap: true,
-          itemBuilder: ((context, index) {
-            return ListTile(
-              leading: CircleAvatar(
-                backgroundImage:
-                    NetworkImage(info[index]["profilePic"].toString()),
+    return ListView.builder(
+
+        itemCount: info.length,
+        shrinkWrap: true,
+        itemBuilder: ((context, index) {
+          return Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage:
+                        NetworkImage(info[index]["profilePic"].toString()),
+                  ),
+                  title: Text(info[index]["name"].toString()),
+                  subtitle: Padding(
+                    padding: const EdgeInsets.only(top: 4),
+                    child: Text(
+                      info[index]["message"].toString(),
+                      style: const TextStyle(color: Colors.grey, fontSize: 14),
+                    ),
+                  ),
+                  trailing: Text(info[index]["time"].toString(),style: const TextStyle(color: Colors.grey, fontSize: 12),),
+                  onTap: null, //TODO LEAD TO CHAT SCREEN
+                ),
               ),
-              title: Text(info[index]["name"].toString()),
-              subtitle: Text(
-                info[index]["message"].toString(),
-                style: const TextStyle(color: Colors.grey, fontSize: 14),
-              ),
-              trailing: Text(info[index]["time"].toString(),style: const TextStyle(color: Colors.grey, fontSize: 12),),
-              onTap: null, //TODO LEAD TO CHAT SCREEN
-            );
-          })),
-    );
+              const Divider(
+                color: dividerColor,
+              )
+              
+            ],
+          );
+        }));
   }
 }
--- lib/screens/web_layout.dart
+++ lib/screens/web_layout.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:whatsapp_ui_clone/screens/contacts_list.dart';
 
 class WebLayout extends StatelessWidget {
   const WebLayout({super.key});
@@ -8,8 +7,31 @@ class WebLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("WEB"),
-      //TODO Build web Layout
-    );
+        body: Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          child: SingleChildScrollView(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: const [
+                  //TODO appbar
+                  //TODO searchbar
+                  ContactList()
+                ]),
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width * 0.7,
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/backgroundImage.png"),
+                  fit: BoxFit.cover)),
+          child: const Center(child: Text("Chats will show up here")),
+          //TODO add chats screen 
+        ),
+      ],
+    ));
   }
 }
